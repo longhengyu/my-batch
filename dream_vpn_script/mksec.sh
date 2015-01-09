8820841e8382afc5ec1708dba93a40fabd3bcf5c
longhengyu <longhengyu@gmail.com>
2015-01-09 23:36:26 +0800

    make it working with vps001

--- dream_vpn_script/mksec.sh
+++ dream_vpn_script/mksec.sh
@@ -2,17 +2,27 @@
 
 sec_dir=/etc/ppp
 work_dir=$(pwd)
-service="http://dream-pkg.cloudfoundry.com/secure/chapSec"
+service="http://dream-vpn.com/secure/chapSec"
 secKey="abcdefg"
 hostname=$(hostname)
 
-requestUrl="${server}/${secKey}/${hostname}"
+requestUrl="${service}/${secKey}/${hostname}"
 
 cd $work_dir
-cp $hostname ${hostname}.backup
-wget requestUrl
+cp ${sec_dir}/chap-secrets ${hostname}.backup
+wget $requestUrl -O $hostname
+read -r format < vps001.backup
+read -r header < vps001
+
+if [ "$header" != "$format" ]; then
+        exit 1
+fi
+
+cat master >> $hostname
 diff $hostname ${hostname}.backup > /dev/null
 v1=$?
 if [ $v1 -ne 0 ]; then
-	cp $hostname ${sec/dir}/chap-secrets
+    cp $hostname ${sec_dir}/chap-secrets
+    cp $hostname.backup $work_dir/backup/$hostname.backup.$(date +"%Y%m%d-%H%M%S")
 fi
+
